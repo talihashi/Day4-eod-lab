d3d50ac7c4910da0439f111cc6a7020a3356aa7c
Jordan Gent <talihashi@gmail.com>
2022-12-15 16:45:43 -0700

    p10

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -97,7 +97,11 @@ let someNum = myFavoriteNumbers[4];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-
+if (myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array.");
+} else {
+  someNum = myFavoriteNumbers[6];
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -108,7 +112,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for (let k = 0; k < listOfNumbers.length; k++) {
+  if (listOfNumbers[k] % 3 === 0) {
+    console.log(`${listOfNumbers[k]} is divisble by 3.`);
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -120,7 +128,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (let b = letters.length; b >= 0; b--) {
+  console.log(letters[b]);
+}
 
 ////////// Advanced Problems //////////
 
@@ -142,7 +152,25 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch (letterGrade) {
+  case "A":
+    console.log("The student is doing excellently.");
+    break;
+  case "B":
+    console.log("The student is doing well.");
+    break;
+  case "C":
+    console.log("The student is doing alright.");
+    break;
+  case "D":
+    console.log("The student is not doing very well.");
+    break;
+  case "F":
+    console.log("The student is failing.");
+    break;
+  default:
+    console.log("Not an eligible grade.");
+}
 
 
 ////////// PROBLEM 11 //////////
